0f099ceb6b92655dc107722a387d706856192b60
Sathish1496 <sathishthiyagaraj2661@gmail.com>
2023-09-17 02:14:43 +0530

    Updated changes

--- force-app/main/default/lwc/calculateFuelDisplay/calculateFuelDisplay.js
+++ force-app/main/default/lwc/calculateFuelDisplay/calculateFuelDisplay.js
@@ -1,13 +1,68 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import dataSelected from "@salesforce/messageChannel/PetrolCalculation__c";
+import {
+  subscribe,
+  MessageContext,
+  APPLICATION_SCOPE
+} from "lightning/messageService";
 
 export default class CalculateFuelDisplay extends LightningElement {
-  /* Daily Fuel Cost
+  @wire(MessageContext)
+  context;
+
+  distance = 0;
+  mileage = 0;
+  fuelCost = 0;
+  dailyFuelCost;
+  monthlyFuelCost;
+  annualFuelCost;
+
+  connectedCallback() {
+    this.subscribeMessage();
+  }
+  subscribeMessage() {
+    // subscribe(messageContext, messageChannel, listener, subscriberOptions)
+    subscribe(
+      this.context,
+      dataSelected,
+      (message) => {
+        this.handleMessage(message);
+      },
+      { scope: APPLICATION_SCOPE }
+    );
+  }
+
+  handleMessage(message) {
+    this.distance = message.distance.value;
+    this.mileage = message.mileage.value;
+    this.fuelCost = message.fuelCost.value;
+
+    this.calculateDailyFuelCost();
+  }
+
+  calculateDailyFuelCost() {
+    /* Daily Fuel Cost
 Fuel Amount = Distance (500 KM) / Mileage (18 KM/l)
 Cost/day = Fuel Amount (27.7) * Fuel Price per liter (102.6) */
-  /* Monthly Fuel Cost
+
+    if (this.distance && this.mileage && this.fuelCost) {
+      this.dailyFuelCost = Math.floor(
+        (this.distance / this.mileage) * this.fuelCost
+      ).toLocaleString("en-US");
+      this.monthlyFuelCost = Math.floor(
+        ((this.distance * 30) / this.mileage) * this.fuelCost
+      ).toLocaleString("en-US");
+      this.annualFuelCost = Math.floor(
+        ((this.distance * 365) / this.mileage) * this.fuelCost
+      ).toLocaleString("en-US");
+    }
+
+    /* Monthly Fuel Cost
 Fuel Amount = (Distance (500 KM) * Number of days per mont = 30days)/ Mileage (18 KM/l)
 Cost/month = Fuel Amount (833.3) * Fuel Price per liter (102.6) */
-  /*Yearly Fuel Cost
+
+    /*Yearly Fuel Cost
 Fuel Amount = (Distance (500 KM) * Number of days per month = 365days)/ Mileage (18 KM/l)
 Cost/year = Fuel Amount (833.3) * Fuel Price per liter (102.6) */
+  }
 }
--- force-app/main/default/lwc/petrolCalculation/petrolCalculation.js
+++ force-app/main/default/lwc/petrolCalculation/petrolCalculation.js
@@ -1,11 +1,44 @@
-import { LightningElement, api, track } from "lwc";
+import { LightningElement, api, wire } from "lwc";
+import dataSelected from "@salesforce/messageChannel/PetrolCalculation__c";
+import { MessageContext, publish } from "lightning/messageService";
 
 export default class PetrolCalculation extends LightningElement {
   @api mileage;
+  @wire(MessageContext)
+  context;
 
-  @track distance;
-  @track selectLocation;
-  @track fuelCost;
+  distance;
+  mileageValue;
+  fuelCost;
 
-  handleCalculate() {}
+  distanceHandler(event) {
+    this.distance = event.target.value;
+    console.log("Distance", event.target.value);
+  }
+
+  mileageHandler(event) {
+    this.mileageValue = event.target.value;
+    console.log("Mileage", event.target.value);
+  }
+
+  costHandler(event) {
+    this.fuelCost = event.target.value;
+    console.log("Fuel Cost", event.target.value);
+  }
+
+  handleCalculate() {
+    const message = {
+      distance: {
+        value: this.distance
+      },
+      mileage: {
+        value: this.mileageValue
+      },
+      fuelCost: {
+        value: this.fuelCost
+      }
+    };
+    // publish(messageContext, messageChannel, message)
+    publish(this.context, dataSelected, message);
+  }
 }
